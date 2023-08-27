17117fe4e2f103cd4c2ad81a2b1943ea82e5ca17
Darian <brooksd@alumni.stanford.edu>
2023-08-26 18:14:52 -0700

    q/a seeder completed

--- app/seeds/__init__.py
+++ app/seeds/__init__.py
@@ -2,6 +2,8 @@ from flask.cli import AppGroup
 from .users import seed_users, undo_users
 from .businesses import seed_businesses, undo_businesses
 from .business_images import seed_business_images, undo_business_images
+from .questions import seed_questions, undo_questions
+from .answers import seed_answers, undo_answers
 
 from app.models.db import db, environment, SCHEMA
 
@@ -14,17 +16,21 @@ seed_commands = AppGroup('seed')
 @seed_commands.command('all')
 def seed():
     if environment == 'production':
-        # Before seeding in production, you want to run the seed undo 
-        # command, which will  truncate all tables prefixed with 
+        # Before seeding in production, you want to run the seed undo
+        # command, which will  truncate all tables prefixed with
         # the schema name (see comment in users.py undo_users function).
         # Make sure to add all your other model's undo functions below
         undo_users()
         undo_businesses()
         undo_business_images()
+        undo_questions()
+        undo_answers()
     seed_users()
     # Add other seed functions here
     seed_businesses()
     seed_business_images()
+    seed_questions()
+    seed_answers()
 
 
 # Creates the `flask seed undo` command
@@ -34,3 +40,5 @@ def undo():
     # Add other undo functions here
     undo_businesses()
     undo_business_images()
+    undo_questions()
+    undo_answers()
