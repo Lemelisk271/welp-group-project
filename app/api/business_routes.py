0b9f038a218b42dd1cf6f0af07f0fc2c6fd09048
Darian <brooksd@alumni.stanford.edu>
2023-08-28 08:59:01 -0700

    business detail page draft (data only) completed

--- app/api/business_routes.py
+++ app/api/business_routes.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, jsonify, request
-from app.models import db, Business, business_amenities, business_hours, Amenity, Category, BusinessImages, Day, Question, Review
+from app.models import db, Business, business_amenities, business_categories, business_hours, Amenity, Category, BusinessImages, Day, Question, Answer, Review
 
 business_routes = Blueprint('businesses', __name__)
 
@@ -8,6 +8,7 @@ business_routes = Blueprint('businesses', __name__)
 def getSingleBusiness(id):
     curr_business = Business.query.get(id)
     business_reviews = Review.query.filter(Review.businessId == id).all()
+    business_images = BusinessImages.query.filter(BusinessImages.businessId == id).all()
 
     business_amenities_join = db.session.query(Amenity).join(
         business_amenities,
@@ -19,15 +20,32 @@ def getSingleBusiness(id):
         business_hours.c.dayId == Day.id
     ).filter(business_hours.c.businessId == id).all()
 
+    business_categories_join = db.session.query(Category).join(
+        business_categories,
+        business_categories.c.categoryId == Category.id
+    ).filter(business_categories.c.businessId == id).all()
+
+    business_questions = Question.query.filter(Question.businessId == id).all()
+    business_answers = db.session.query(Answer).join(
+        Question,
+        Question.id == Answer.questionId
+    ).filter(Question.businessId == id).all()
+
     biz_dict = curr_business.to_dict()
     reviews_dict = [review.to_dict() for review in business_reviews]
     amenities_dict = [amenity.to_dict() for amenity in business_amenities_join]
-    hours_dict = [day.to_dict() for day in business_days_join]
+    hours_dict = [{
+            "id": day.id,
+            "day": day.day,
+            "open_time": day.open_time.strftime("%H:%M:%S") if day.open_time else None,
+            "close_time": day.close_time.strftime("%H:%M:%S") if day.close_time else None
+        } for day in business_days_join]
+    categories_dict = [category.to_dict() for category in business_categories_join]
+    questions_dict = [question.to_dict() for question in business_questions]
+    for question in questions_dict:
+        question_id = question["id"]
+        question_answers = [answer.to_dict() for answer in business_answers if answer.questionId == question_id]
+        question["answers"] = question_answers
+    images_dict = [image.to_dict() for image in business_images]
 
-    return {**biz_dict, "reviews": reviews_dict, "amenities": amenities_dict, "hours": hours_dict}
-
-
-#  business_categories = Category.query.filter(Category.businessId == id).all()
-#     business_questions = Question.query.filter(Question.businessId == id).all()
-#     business_images = BusinessImages.query.filter(BusinessImages.businessId == id).all()
-#     business_days = Day.query.filter(Day.businessId == id).all()
+    return {**biz_dict, "reviews": reviews_dict, "images": images_dict, "amenities": amenities_dict, "hours": hours_dict, "categories": categories_dict, "questions": questions_dict}
--- react-app/src/components/Businesses/BusinessDetails.js
+++ react-app/src/components/Businesses/BusinessDetails.js
@@ -7,9 +7,9 @@ import "./Businesses.css";
 const BusinessDetails = () => {
   const dispatch = useDispatch();
   const [isLoaded, setIsLoaded] = useState(false);
-//   const [business, setBusiness] = useState(null);
+  //   const [business, setBusiness] = useState(null);
   const { id } = useParams();
-  const business = useSelector(state => state.business.singleBusiness)
+  const business = useSelector((state) => state.business.singleBusiness);
 
   useEffect(() => {
     try {
@@ -18,24 +18,111 @@ const BusinessDetails = () => {
     } catch (error) {
       console.log(error);
     }
-}, dispatch);
+  }, dispatch);
 
-return (
+  return (
     <>
       <h1>Business Details</h1>
-    {business && console.log("BUSINESS", business)}
+      {business && console.log("BUSINESS", business)}
       {business && <h2>Name: {business.name}</h2>}
-      {business && <h2>url: {business.url}</h2>}
-      {business && <h2>phone: {business.phone}</h2>}
+      {business && <h2>Website: {business.url}</h2>}
+      {business && <h2>Phone: {business.phone}</h2>}
       {business && (
         <h2>
-          address: {business.address} {business.city}, {business.state}{" "}
+          Address: {business.address} {business.city}, {business.state}{" "}
           {business.zip_codes}
         </h2>
       )}
-      {business && <h2>about: {business.about}</h2>}
-      {business && <h2>price: {business.price}</h2>}
-      {business && <h2>owner: {business.ownerId}</h2>}
+      {business && <h2>About: {business.about}</h2>}
+      {business && <h2>Price: {business.price}</h2>}
+      {business && <h2>Owner: {business.ownerId}</h2>}
+      {business && (
+        <h2>
+          Images:{" "}
+          {business.images.map((element) => {
+            return (
+              <ul>
+                <li key={element.id}>
+                  {element.url}
+                </li>
+              </ul>
+            );
+          })}
+        </h2>
+      )}
+      {business && (
+        <h2>
+          Reviews:{" "}
+          {business.reviews.map((element) => {
+            return (
+              <ul>
+                <li key={element.id}>
+                  {element.date} - Stars: {element.stars}
+                </li>
+              </ul>
+            );
+          })}
+        </h2>
+      )}
+      {business && (
+        <h2>
+          Hours:{" "}
+          {business.hours.map((element) => {
+            return (
+              <ul>
+                <li key={element.id}>
+                  {element.day} -{" "}
+                  {(element.open_time && element.open_time + " -") || "Closed"}{" "}
+                  {element.close_time}
+                </li>
+              </ul>
+            );
+          })}
+        </h2>
+      )}
+      {business && (
+        <h2>
+          Amenities:{" "}
+          {business.amenities.map((element) => {
+            return (
+              <ul>
+                <li key={element.id}>{element.amenity}</li>
+              </ul>
+            );
+          })}
+        </h2>
+      )}
+      {business && (
+        <h2>
+          Categories:{" "}
+          {business.categories.map((element) => {
+            return (
+              <ul>
+                <li key={element.id}>{element.category}</li>
+              </ul>
+            );
+          })}
+        </h2>
+      )}
+      {business && (
+        <h2>
+          Questions:{" "}
+          {business.questions.map((element) => {
+            return (
+              <ul>
+                <li key={element.id}>
+                  {element.question}
+                  {element.answers.length > 0 && (
+                    <ul>
+                      <li key={element.answers.id}>{element.answers[0].answer}</li>
+                    </ul>
+                  )}
+                </li>
+              </ul>
+            );
+          })}
+        </h2>
+      )}
     </>
   );
 };
