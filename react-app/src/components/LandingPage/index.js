bfaf5e9d0282de7179b0ebad6e2fc10654397070
Daniel Lewis <daniel.c.lewis@outlook.com>
2023-08-28 14:46:59 -0500

    Created basic card layout for landing page

--- react-app/src/components/LandingPage/index.js
+++ react-app/src/components/LandingPage/index.js
@@ -6,7 +6,20 @@ export default function LandingPage() {
         <>
         <div className="landing-page hero-bar">
         </div>
-        <h1>LandingPage</h1>
+        <div className="landing-page card-container">
+            <h2 className="landing-page-card-header">Your Next Review Awaits</h2>
+            {/* Map over query results for businesses to review */}
+            <div className="landing-page-card">
+                <div className="landing-page-card-image">
+                    <img src="https://placehold.co/200x200.png?text=Preview+Image" />
+                </div>
+                <div className="landing-page-card-details">
+                    <h3>Business Name</h3>
+                    <p>Do you recommend this business?</p>
+                    <p>Insert Star Rating Here</p>
+                </div>
+            </div>
+        </div>
         </>
     )
 }
