c750c78883bdd7b2ca0493e716a306fca3103ed9
Daniel Lewis <daniel.c.lewis@outlook.com>
2023-08-27 21:41:08 -0500

    Created landing page wireframe for navigation bar and hero bar

--- react-app/src/components/Navigation/index.js
+++ react-app/src/components/Navigation/index.js
@@ -1,24 +1,36 @@
-import React from 'react';
-import { NavLink } from 'react-router-dom';
-import { useSelector } from 'react-redux';
-import ProfileButton from './ProfileButton';
-import './Navigation.css';
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { NavLink } from "react-router-dom";
+import { useSelector } from "react-redux";
+import ProfileButton from "./ProfileButton";
+import "./Navigation.css";
 
-function Navigation({ isLoaded }){
-	const sessionUser = useSelector(state => state.session.user);
+function Navigation({ isLoaded }) {
+  const sessionUser = useSelector((state) => state.session.user);
 
-	return (
-		<ul>
-			<li>
-				<NavLink exact to="/">Home</NavLink>
-			</li>
-			{isLoaded && (
-				<li>
-					<ProfileButton user={sessionUser} />
-				</li>
-			)}
-		</ul>
-	);
+  return (
+	<div className="navigation hero-bar">
+    <div className="navigation nav-container">
+      <div className="navigation nav-left">
+        <NavLink exact to="/">
+          <img
+            className="navigation nav-logo"
+            src="https://placehold.co/80x40/000000/FFFFFF.png"
+          />
+        </NavLink>
+      </div>
+      <div className="navigation nav-center">
+		<input className="navigation nav-search-bar" />
+		<button>Search</button>
+      </div>
+      <div className="navigation nav-right">
+          {isLoaded && (
+              <ProfileButton user={sessionUser} />
+          )}
+      </div>
+    </div>
+	</div>
+  );
 }
 
 export default Navigation;
