3a69358c2a9b98d29c3987b7446fafe40b87dea3
Daniel Lewis <daniel.c.lewis@outlook.com>
2023-08-29 11:42:03 -0500

    Pulled business review with temporary fetch request, organizing fetch results in to Recent Activity cards

--- react-app/src/components/LandingPage/index.js
+++ react-app/src/components/LandingPage/index.js
@@ -1,36 +1,55 @@
-import "./LandingPage.css"
+import { useEffect, useState } from "react";
+import "./LandingPage.css";
 
 export default function LandingPage() {
-    return (
-        <>
-        <div className="landing-page hero-bar">
-        </div>
-        <div className="landing-page-card-container">
-            <h2 className="landing-page-card-header">Recent Activity</h2>
-            {/* Map over query results for recent reviews */}
+  const [recentActivity, setRecentActivity] = useState([]);
+
+  useEffect(() => {
+    const getRecentActivity = async () => {
+      const businesses = await fetch(`/api/business/16`);
+      const businessesData = await businesses.json();
+      setRecentActivity(businessesData.reviews);
+      console.log("Recent Activity ==>", recentActivity);
+    };
+    getRecentActivity();
+  }, []);
+
+  return (
+    <>
+      <div className="hero-bar"></div>
+      <div className="landing-page-card-container">
+        <h2 className="landing-page-card-header">Recent Activity</h2>
+        {/* Map over query results for recent reviews */}
+        {recentActivity.map(({businessId, stars, review, userId}) => (
+            // console.log("Mapped Activity ==>", activity)
+          <>
             <div className="landing-page-card">
-                <div className="landing-page-card-header">
-                    <h4>Username</h4>
-                    <p>Wrote a review</p>
-                </div>
-                <img className="landing-page-card-image" src="https://picsum.photos/200/300.jpg" alt="Landing Page" />
-                <div className="landing-page-card-details">
-                    <h4>Business Name</h4>
-                    <p>Star Rating</p>
-                    <p>Review Here</p>
-                </div>
+              <div className="landing-page-card-header">
+                <h4>{userId}</h4>
+                <p>Wrote a review</p>
+              </div>
+              <img
+                className="landing-page-card-image"
+                src="https://picsum.photos/200/300.jpg"
+                alt="Landing Page"
+              />
+              <div className="landing-page-card-details">
+                <h4>{businessId}</h4>
+                <p>{stars}</p>
+                <p>{review}</p>
+              </div>
             </div>
+          </>
+        ))}
+      </div>
+      <hr></hr>
+      <div className="landing-page-categories-container">
+        {/* Map over query results for categories */}
+        <h2 className="landing-page-categories-header">Categories</h2>
+        <div className="landing-page-categories-buttons">
+          <div className="landing-page-category">Restaurants</div>
         </div>
-        <hr></hr>
-        <div className="landing-page-categories-container">
-            {/* Map over query results for categories */}
-            <h2 className="landing-page-categories-header">Categories</h2>
-            <div className="landing-page-categories-buttons">
-                <div className="landing-page-category">
-                    Restaurants
-                </div>
-            </div>
-        </div>
-        </>
-    )
+      </div>
+    </>
+  );
 }
