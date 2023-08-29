596134964ec694c5b00b115021bdd3cf84fda13b
Zach Smith <zwsmith27@gmail.com>
2023-08-28 20:31:41 -0700

    fixed zip code helper function

--- app/api/business_routes.py
+++ app/api/business_routes.py
@@ -4,6 +4,14 @@ from ..forms import BusinessForm
 
 business_routes = Blueprint('businesses', __name__)
 
+@business_routes.route("/")
+def all_businesses():
+    """
+    Query for all businesses and returns them in a list of business dictionaries
+    """
+    businesses = Business.query.all()
+    return {"businesses": [business.to_dict() for business in businesses]}
+
 
 @business_routes.route("/new", methods=["POST"])
 def createNewBusiness():
