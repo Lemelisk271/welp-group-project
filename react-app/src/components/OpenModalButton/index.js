0a2804b82ad5469ae7e381cc18bf603972a7803e
Daniel Lewis <daniel.c.lewis@outlook.com>
2023-08-31 15:24:56 -0500

    Fine tuning nav bar styling for home

--- react-app/src/components/OpenModalButton/index.js
+++ react-app/src/components/OpenModalButton/index.js
@@ -1,23 +1,39 @@
-import React from 'react';
-import { useModal } from '../../context/Modal';
+import React from "react";
+import { useModal } from "../../context/Modal";
 
 function OpenModalButton({
-  modalComponent, // component to render inside the modal
-  buttonText, // text of the button that opens the modal
-  onButtonClick, // optional: callback function that will be called once the button that opens the modal is clicked
-  onModalClose // optional: callback function that will be called once the modal is closed
+    modalComponent, // component to render inside the modal
+    buttonText, // text of the button that opens the modal
+    onButtonClick, // optional: callback function that will be called once the button that opens the modal is clicked
+    onModalClose, // optional: callback function that will be called once the modal is closed
 }) {
-  const { setModalContent, setOnModalClose } = useModal();
+    const { setModalContent, setOnModalClose } = useModal();
 
-  const onClick = () => {
-    if (onModalClose) setOnModalClose(onModalClose);
-    setModalContent(modalComponent);
-    if (onButtonClick) onButtonClick();
-  };
+    const onClick = () => {
+        if (onModalClose) setOnModalClose(onModalClose);
+        setModalContent(modalComponent);
+        if (onButtonClick) onButtonClick();
+    };
 
-  return (
-    <button className="nav-login-button delete-business" onClick={onClick}>{buttonText}</button>
-  );
+    if (buttonText === "Sign Up") {
+        return (
+            <button
+                className="big-red-button"
+                onClick={onClick}
+            >
+                {buttonText}
+            </button>
+        );
+    } else {
+      return (
+        <button
+            className="nav-login-button delete-business"
+            onClick={onClick}
+        >
+            {buttonText}
+        </button>
+    );
+    }
 }
 
 export default OpenModalButton;
