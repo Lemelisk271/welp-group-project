7a6883d5316b620146d32c17055d5f06407e6b30
Daniel Lewis <daniel.c.lewis@outlook.com>
2023-08-31 11:57:58 -0500

    Created recent activity route to return landing-page infor for recent activity cards

--- react-app/src/components/LandingPage/index.js
+++ react-app/src/components/LandingPage/index.js
@@ -6,10 +6,10 @@ export default function LandingPage() {
 
   useEffect(() => {
     const getRecentActivity = async () => {
-      const businesses = await fetch(`/api/business/16`);
-      const businessesData = await businesses.json();
-      setRecentActivity(businessesData.reviews);
-      console.log("Recent Activity ==>", recentActivity);
+      const reviews = await fetch(`/api/review/recent`);
+      const reviewData = await reviews.json();
+      setRecentActivity(reviewData.reviews);
+      // console.log("Recent Activity ==>", recentActivity);
     };
     getRecentActivity();
     // eslint-disable-next-line
@@ -19,14 +19,14 @@ export default function LandingPage() {
     <>
       <div className="hero-bar"></div>
       <div className="landing-page-card-container">
-        <h2 className="landing-page-card-header">Recent Activity</h2>
+          <h2 className="no-margin">Recent Activity</h2>
         <div className="landing-page-activity-cards">
           {/* Map over query results for recent reviews */}
-          {recentActivity.map(({ businessId, stars, review, userId }) => (
+          {recentActivity.map(({ businessName, userName, stars, review, date }) => (
             // console.log("Mapped Activity ==>", activity)
-            <div className="landing-page-card" key={userId}>
+            <div className="landing-page-card" key={userName}>
               <div className="landing-page-card-header">
-                <h4>{userId}</h4>
+                <h4>{userName}</h4>
                 <p>Wrote a review</p>
               </div>
               <img
@@ -35,7 +35,8 @@ export default function LandingPage() {
                 alt="Landing Page"
               />
               <div className="landing-page-card-details">
-                <h4>{businessId}</h4>
+                <h4>{businessName}</h4>
+                <p>{date}</p>
                 <p>{stars}</p>
                 <p>{review.substring(0,80)}...</p>
               </div>
